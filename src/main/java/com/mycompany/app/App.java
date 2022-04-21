cdfcfd64727168276cc4f731a1a38c8f8cb8ad3f
tanisha-ION <104118807+tanisha-ION@users.noreply.github.com>
2022-04-21 09:19:59 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,13 @@
 package com.mycompany.app;
 
-//import com.github.tomaslanger.chalk.Chalk;
+import com.github.tomaslanger.chalk.Chalk;
 
 public class App 
 {
     public static void main( String[] args )
     {
-    	Printer p = new Printer();
-    	p.print("Printing");
-    	//System.out.println("This message is " + Chalk.on("IMPORTANT").red().underline());
+    	//Printer p = new Printer();
+    	//p.print("Printing");
+    	System.out.println("This message is " + Chalk.on("IMPORTANT").red().underline());
     }
 }
